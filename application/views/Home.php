5f88406b1eb2b8f474a24a5ff0064f3b1a91f65d
Jhonathan Silva <jhow.pds@hotmail.com>
2018-06-06 13:55:13 +0000

    commit inicial

---
+++ application/views/Home.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Home - Organizador de Tarefas </title>
+</head>
+<body>
+
+<div id="container">
+	<h1>Organizador de Tarefas</h1>
+    <form method="" action="">
+        <label for="login">Login</label>
+        <input type="text" name="login"/>
+        <label for="senha">Senha</label>
+        <input type="password" name="senha"/>
+        <input type="submit" value="Entrar"/>
+    </form>
+</div>
+
+</body>
+</html>
