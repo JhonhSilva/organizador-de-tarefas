fa58ff66df614c9014b2c411705ff5e00e66f535
Jhonathan Silva <jhow.pds@hotmail.com>
2018-06-06 15:00:39 +0000

    Estruturação inicial

---
+++ application/views/Tarefa.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Tarefa - Organizador de Tarefas </title>
+</head>
+<body>
+
+<div id="container">
+	<h1>Organizador de Tarefas</h1>
+    <form method="" action="">
+        
+    </form>
+</div>
+
+</body>
+</html>
