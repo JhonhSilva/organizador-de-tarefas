633dc7c67cc2e9c28a36e672d3c58004d312c572
Gabriel Craveiro <gabriel.c.craveiro@gmail.com>
2018-06-10 14:14:59 +0000

    Criação das views e inclusão de css

---
+++ application/views/LoginView.php
@@ -0,0 +1,60 @@
+<body>
+
+	<div class="navbar navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container">
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
+					<i class="icon-reorder shaded"></i>
+				</a>
+
+			  	<a class="brand" href="index.html">
+			  		Organizador de Tarefas
+			  	</a>
+
+				<div class="nav-collapse collapse navbar-inverse-collapse">
+				
+					<ul class="nav pull-right">
+
+						<li><a href="#">
+                        Cadastrar
+                        </a></li>
+					</ul>
+				</div><!-- /.nav-collapse -->
+			</div>
+		</div><!-- /navbar-inner -->
+	</div><!-- /navbar -->
+
+
+
+	<div class="wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="module module-login span4 offset4">
+					<form class="form-vertical" method="POST" action="usuario/logar">
+						<div class="module-head">
+							<h3>Entrar</h3>
+						</div>
+						<div class="module-body">
+							<div class="control-group">
+								<div class="controls row-fluid">
+									<input class="span12" type="text" id="inputEmail" name="login" placeholder="Login">
+								</div>
+							</div>
+							<div class="control-group">
+								<div class="controls row-fluid">
+									<input class="span12" type="password" id="inputPassword" name="senha" placeholder="Senha">
+								</div>
+							</div>
+						</div>
+						<div class="module-foot">
+							<div class="control-group">
+								<div class="controls clearfix">
+									<button type="submit" value="Entrar" class="btn btn-primary pull-right">Entrar</button>
+								</div>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div><!--/.wrapper-->
---
+++ application/views/includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <title>Organizador de tarefas</title>
+        
+        <link type="text/css" href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link type="text/css" href="/assets/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+        <link type="text/css" href="/assets/css/theme.css" rel="stylesheet">
+        <link type="text/css" href="/assets/images/icons/css/font-awesome.css" rel="stylesheet">
+        <link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600'
+            rel='stylesheet'>
+
+  </head>
+
+  <body>
