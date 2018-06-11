bc89feb88c9b197169cad4b1476ba6baea7d5d60
Jhonathan Silva <jhow.pds@hotmail.com>
2018-06-11 04:00:14 +0000

    Criação de View para cadastro de usuário

---
+++ application/views/UsuarioCadastrarView.php
@@ -0,0 +1,65 @@
+<body>
+
+	<div class="navbar navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container">
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
+					<i class="icon-reorder shaded"></i>
+				</a>
+
+			  	<a class="brand" href="index.html">
+			  		Organizador de Tarefas
+			  	</a>
+
+				<div class="nav-collapse collapse navbar-inverse-collapse">
+				
+					<ul class="nav pull-right">
+
+						<li><a href="usuario/logar">
+                        Login
+                        </a></li>
+					</ul>
+				</div><!-- /.nav-collapse -->
+			</div>
+		</div><!-- /navbar-inner -->
+	</div><!-- /navbar -->
+
+
+
+	<div class="wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="module module-login span4 offset4">
+					<form class="form-vertical" method="POST" action="usuario/cadastrar">
+						<div class="module-head">
+							<h3>Cadastrar Usuário</h3>
+						</div>
+						<div class="module-body">
+							<div class="control-group">
+								<div class="controls row-fluid">
+									<input class="span12" type="email" id="inputEmail" name="login" placeholder="Login" required>
+								</div>
+							</div>
+							<div class="control-group">
+								<div class="controls row-fluid">
+									<input class="span12" type="password" id="inputPassword" name="senha" placeholder="Senha" required>
+								</div>
+							</div>
+							<div class="control-group">
+								<div class="controls row-fluid">
+									<input class="span12" type="password" id="inputPassword" name="senha2" placeholder="Confirme a senha" required>
+								</div>
+							</div>
+						</div>
+						<div class="module-foot">
+							<div class="control-group">
+								<div class="controls clearfix">
+									<button type="submit" value="Entrar" class="btn btn-primary pull-right">Cadastrar</button>
+								</div>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div><!--/.wrapper-->
