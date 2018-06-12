6b0c59bb80efb3680e4b4bcbdb77422b6af98e5c
Jhonathan Silva <jhow.pds@hotmail.com>
2018-06-12 01:11:25 +0000

    Criação do getter de id para a entidade Aluno

--- application/models/Usuario.php
+++ application/models/Usuario.php
@@ -2,13 +2,19 @@
     class Aluno {
         private $id, $nome, $email, $senha;
         
-        // criação do contrutor da classe Aluno
+        // contrutor da classe Aluno
         // Jhonathan Silva
         public function Aluno($nome, $email, $senha, $id=0) {
             $this->id = $id;
             $this->nome = $nome;
             $this->email = $email;
             $this->senha = $senha;
+        }
+        
+        // getter do atributo id
+        // Jhonathan Silva
+        public function getId() {
+            return $this->id;
         }
     }
 ?>
