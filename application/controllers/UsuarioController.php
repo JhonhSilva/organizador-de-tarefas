32d8fc9e94177af25c859159a6c62adc01d969f3
Jhonathan Silva <jhow.pds@hotmail.com>
2018-06-12 04:20:15 +0000

    criação de função para edição de Usuario

--- application/controllers/UsuarioController.php
+++ application/controllers/UsuarioController.php
@@ -25,16 +25,50 @@ class UsuarioController extends CI_Controller {
     // Jhonathan Silva
 	public function Cadastrar() 
 	{
+		$this->load->library('form_validation');
+
 		$nome = $this->input->post("nome");
 	    $email = $this->input->post("email");
 	    $senha = $this->input->post("senha");
 	    
-	    if(isset($nome) and isset($email) and isset($senha)) {
-	    	$usuario = new Usuario($nome, $email, $senha);
-	    	$this->Usuario_model->insert($usuario);
+
+        $this->form_validation->set_rules('email', 'email', 'is_unique[usuario.usuario_email]');
+	    
+	    if (!$this->form_validation->run()) {
+            $this->session->set_flashdata('msgErro', 'O email já existe');
+            redirect('usuario/cadastro');
+		} else {
+	    	if(isset($nome) and isset($email) and isset($senha)) {
+		    	$usuario = new Usuario($nome, $email, $senha);
+		    	$this->Usuario_model->insert($usuario);
+		    	
+		    	$dados = array();
+		    	$dados['login'] = $usuario->getEmail();
+		    	$dados['senha'] = $usuario->getSenha();
+		    	
+		    	redirect('usuario/logar', $dados);
+			}
 	    }
-	    else {
-	    	echo '<h1>Error</h1>';
-	    }
+    }
+    
+    // função para editar usuário
+    // Jhonathan Silva
+    public function Edicao() {
+    	
+    	$id = $this->session->userdata['usuario_id'];
+    	
+    	if(isset($id)) {
+    		$usuario = $this->Usuario_model->select($id);
+    		$dados = array();
+    		$dados['nome'] = $usuario["usuario_nome"];
+    		$dados['email'] = $usuario["usuario_email"];
+    		
+    		$this->load->view('includes/header');
+			$this->load->view('UsuarioEditarView', $dados);
+			$this->load->view('includes/footer');
+    	}
+    	else {
+    		echo "<h1>Deu ruim!</h1>";
+    	}
     }
 }
